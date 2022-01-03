dc8488056ae65e0968ccb6e111ba61aa6b250a0e
Nova <eldanadesalgne@gmail.com>
2022-01-02 20:34:15 -0500

    attached the api to the current and fixed the buttons

--- script.js
+++ script.js
@@ -5,12 +5,11 @@ var apiKey = "8dc615c0e18f5961506109a55060c458";
 //Identifying Elements from HTML 
 var searchBTNEL = $('.searchBTN');
 var searchBarEL = $("#searchBAR");
-console.log(searchBarEL)
 
-function btnOPS (){
-  searchBTNEL.on("click",function(event){
+//SEARCH BUTTON 
+function btnOPS() {
+  searchBTNEL.on("click", function (event) {
     event.preventDefault();
-    console.log("Sub Button Works!");
     cityNameP = searchBarEL.val();
     console.log(cityNameP);
     callAPI();
@@ -19,33 +18,58 @@ function btnOPS (){
 btnOPS();
 
 var newButtonsEL = $(".newButtons");
-//First things first- Call on API and making sure that it works
+var currentDateEL = $(".currentDate");
+var cDnameEL = $(".nameDate");
+var cDtempEL = $(".tempCD");
+var cDwindEL = $(".windCD");
+var cDhumidityEL = $(".humidityCD");
+var cDUVEL = $(".uVCD");
+var uVIndex = 0.0;
 
-function callAPI (){
 
-  var mainURL = "https://api.openweathermap.org/data/2.5/forecast?q="+ cityNameP +"&appid=8dc615c0e18f5961506109a55060c458";
+function callAPI() {
+
+  var mainURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityNameP + "&units=imperial&appid=8dc615c0e18f5961506109a55060c458";
 
   fetch(mainURL)//GET is the default.
-      .then(function (response) {
-        return response.json();
-      })//required 
-      .then(function (data) {
-        localStorage.setItem(cityNameP,JSON.stringify(data));
-        var cityBTNEL = localStorage.getItem(cityNameP);
-        console.log(JSON.parse(cityBTNEL));
-        newButtonsEL.append('<button class="btn btn-primary newCity" type="button">cityName</button>')
-        var newCityBtn = $(".newCity");
-        newCityBtn.text(cityNameP);
-      });
+    .then(function (response) { //GET responses
+      return response.json();
+    })//required 
+    .then(function (data) { //Store this and create buttons in local Storage 
+      localStorage.setItem(cityNameP, JSON.stringify(data));
+      var cityBTNEL = localStorage.getItem(cityNameP);
+      console.log(JSON.parse(cityBTNEL));
+      newButtonsEL.append('<button class="btn btn-primary" type="button">' + cityNameP + '</button>')
+      var newCityBtn = $(".newCity");
+      newCityBtn.text(cityNameP);
+
+      //STORE NEEDED DATA 
+      var nameDate = data.city.name + " (" + moment(data.list[0].dt_txt).format('ll') + ")";
+      var temp = data.list[0].main.temp + " °F";
+      var wind = data.list[0].wind.speed + "MPH";
+      var humidity = data.list[0].main.humidity + "%";
+      var lat = data.city.coord.lat;
+      var lon = data.city.coord.lon;
+      uv(lat,lon);
+      //PRINT ON CARD 
+      cDnameEL.text(nameDate);
+      cDtempEL.text("Temperature: " +temp);
+      cDwindEL.text("Wind Speed: " +wind);
+      cDhumidityEL.text("Humidity: " +humidity);
+      cDUVEL.text("UV Index: " +uVIndex);
+    });
 }
+function uv(lat,lon) {
+  var uvURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&units=imperial&appid=8dc615c0e18f5961506109a55060c458";
 
-
-// //Here is the funcation that can get the data from local storage and display it on to the screen
-// function show(){
-//   for (let i = 9; i < 18; i++) {
-//     var keyVal = localStorage.getItem(i); //getting local storage only the Value 
-//     var timeBlockSection = $("#" + i).children().eq(1); //calling in the ID from the area 
-//     timeBlockSection.val(keyVal);//attaching the value to show this as display
-//   }
-// }
-// show();//constant call on function 
+  fetch(uvURL)
+    .then(function (response) { //GET responses
+      return response.json();
+    })//required 
+    .then(function (data) {
+      console.log(data);
+      uVIndex = data.current.uvi;
+      console.log(uVIndex);
+    });
+  console.log("UV index: " + uVIndex);
+}
